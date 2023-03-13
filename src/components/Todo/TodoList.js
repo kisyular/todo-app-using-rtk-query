990dfb5624fd32cfafacbdff8d0fd2580c34a950
kisyular <rellika@kisyula.com>
2023-03-13 18:01:38 +0300

    ADDED PROVIDER TO INDEX JS

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,31 +1,24 @@
 import React from 'react'
 import Todo from './Todo'
 
+import { useGetTodosQuery } from '../../api/apiSlice'
+import { Error, Loading } from '../Status'
+
 const TodoList = () => {
-	const todos = {
-		todos: [
-			{
-				userId: 3,
-				id: 1,
-				title: 'Revise the employee handbook to include new policies',
-				completed: false,
-				changes: {
-					completed: true,
-				},
-			},
-			{
-				userId: 6,
-				id: 2,
-				title: 'Develop a training program for new employees',
-				completed: false,
-			},
-		],
-	}
+	const {
+		data: todos,
+		isLoading,
+		isSuccess,
+		isError,
+		error,
+	} = useGetTodosQuery()
+
+	if (isLoading) return <Loading />
+	if (isError) return <Error error={error} />
 	return (
 		<div>
-			{todos.todos.map((todo) => (
-				<Todo key={todo.id} todo={todo} />
-			))}
+			{isSuccess &&
+				todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
 		</div>
 	)
 }
